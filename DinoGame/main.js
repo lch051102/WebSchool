d3f66f3927855db261faeb425af3dc8a529e4a23
lch051102 <ichungheon123@gmail.com>
2023-06-12 14:53:37 +0900

    좀 이상한 다이노게임

--- DinoGame/main.js
+++ DinoGame/main.js
@@ -31,3 +31,48 @@ class Cactus {
 
 var cactus = new Cactus();
 cactus.draw();
+
+function 프레임(){
+    requestAnimationFrame(프레임);
+    cactus.x--;
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    dino.draw();
+    cactus.draw();
+  
+}
+  
+function Frame() {
+    if (isUpPressed) {
+      dino.y--;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      dino.draw();
+      cactus.draw();
+    }
+    else if (!isUpPressed) {
+        dino.y++;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        dino.draw();
+        cactus.draw();
+    }
+  
+    requestAnimationFrame(Frame);
+  }
+  
+  let isUpPressed = false;
+  
+  document.addEventListener('keydown', function (event) {
+    if (event.code === 'KeyW') {
+        isUpPressed = true;
+    }
+  });
+  
+  document.addEventListener('keydown', function (event) {
+    if (event.code === 'KeyS') {
+        isUpPressed = false;
+    }
+  });
+  
+  Frame();
+  
+
+프레임();
